e12f6daa0301f7856cb484804652e4dcbe80a01d
louie <louiecasula@gmail.com>
2023-11-15 15:26:24 -0500

    Worked on some of RouletteBoardTest

---
+++ src/test/java/com/github/zipcodewilmington/roulette/RouletteBoardTest.java
@@ -0,0 +1,28 @@
+package com.github.zipcodewilmington.roulette;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class RouletteBoardTest {
+
+    private RouletteBoard board;
+    @Before
+    public void setup() {
+        this.board = new RouletteBoard();
+    }
+
+    @Test
+    public void rouletteBoardConstructorTest() {
+        Assert.assertNotNull(board);
+    }
+
+    @Test
+    public void isRedTest(){
+
+
+        Boolean actual = board.isRed(1);
+    }
+}
---
+++ src/test/java/com/github/zipcodewilmington/roulette/RouletteGameTest.java
@@ -0,0 +1,4 @@
+package com.github.zipcodewilmington.roulette;
+
+public class RouletteGameTest {
+}
