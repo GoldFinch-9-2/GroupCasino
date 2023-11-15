890cda31aeaa0ac00acc6a843fe51c80dbeac3d6
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-15 16:05:31 -0500

    created Face and Suit enums

--- src/main/java/com/github/zipcodewilmington/casino/cards/Suit.java
+++ src/main/java/com/github/zipcodewilmington/casino/cards/Suit.java
@@ -1,4 +1,18 @@
 package com.github.zipcodewilmington.casino.cards;
 
 public enum Suit {
+    HEARTS ("red"),
+    DIAMONDS ("red"),
+    CLUBS ("black"),
+    SPADES ("black");
+
+    String color;
+
+    Suit(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
 }
