d3229297caf7096b66bb385ce8c8d6e08a500419
Gowri <gowri@Zipcoders-MacBook-Pro-64.local>
2023-11-15 09:59:13 -0500

    changes done

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -7,26 +7,26 @@ import java.util.Random;
 /**
  * Created by leon on 7/21/2020.
  */
-public class NumberGuessGame {
-//    int noOfDice;
-//    public NumberGuessGame(int numberOfDice){
-//        this.numberOfDice = numberOfDice;
-//
-//    }
-//    public Integer[] toss(){
-//        Integer dice1 = 0;
-//        Integer dice2 = 0;
-//        int[] numbers = 0;
-//
-//        Random random = new Random();
-//        for(int i = 0; i < numbers.length; i++){
-//            dice1 += random.nextInt(6)+1;
-//            dice2 += random.nextInt(6)+1 ;
-//            numbers[0] = dice1;
-//            numbers[1] = dice2;
-//
-//        }
-//        int numbers1 = (int) numbers;
-//        return numbers1;
-//    }
+public class NumberGuessGame extends Dice{
+    int noOfDice;
+    public NumberGuessGame(int numberOfDice){
+        this.numberOfDice = numberOfDice;
+
+    }
+    public Integer[] toss(){
+        Integer dice1 = 0;
+        Integer dice2 = 0;
+        int[] numbers = 0;
+
+        Random random = new Random();
+        for(int i = 0; i < numbers.length; i++){
+            dice1 += random.nextInt(6)+1;
+            dice2 += random.nextInt(6)+1 ;
+            numbers[0] = dice1;
+            numbers[1] = dice2;
+
+        }
+        int numbers1 = (int) numbers;
+        return numbers1;
+    }
 }
