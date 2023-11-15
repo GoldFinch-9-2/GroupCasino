0293be4caa5ab0d02aa772166ab431782e28070c
Gowri <gowri@Zipcoders-MacBook-Pro-64.local>
2023-11-15 09:46:00 -0500

    changes

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -1,7 +1,32 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
+import java.util.Random;
+
 /**
  * Created by leon on 7/21/2020.
  */
 public class NumberGuessGame {
+//    int noOfDice;
+//    public NumberGuessGame(int numberOfDice){
+//        this.numberOfDice = numberOfDice;
+//
+//    }
+//    public Integer[] toss(){
+//        Integer dice1 = 0;
+//        Integer dice2 = 0;
+//        int[] numbers = 0;
+//
+//        Random random = new Random();
+//        for(int i = 0; i < numbers.length; i++){
+//            dice1 += random.nextInt(6)+1;
+//            dice2 += random.nextInt(6)+1 ;
+//            numbers[0] = dice1;
+//            numbers[1] = dice2;
+//
+//        }
+//        int numbers1 = (int) numbers;
+//        return numbers1;
+//    }
 }
