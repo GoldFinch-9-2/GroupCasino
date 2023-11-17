b87f6d8845b174660ff7a945283ae79c341f2f47
louie <louiecasula@gmail.com>
2023-11-17 00:55:15 -0500

    RouletteGame works (but with minor bugz)

--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
@@ -8,30 +8,25 @@ import com.github.zipcodewilmington.utils.AnsiColor;
 import com.github.zipcodewilmington.utils.IOConsole;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
-public class RouletteGame implements GameInterface, GamblingInterface {
+public class RouletteGame implements GameInterface {
     private final IOConsole console = new IOConsole(AnsiColor.BLUE);
-    CasinoAccount account = new CasinoAccount("user","pass");
-
-    RouletteBoard board;
+    Random random = new Random();
+    ArrayList<RoulettePlayer> players = new ArrayList<>();
+    ArrayList<Integer> redNumbers = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36));
     String currentNumber;
-    ArrayList<String> allNumbers;
-
+    ArrayList<String> allNumbers = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36"));;
+    ArrayList<String> TripleMultiplier = new ArrayList<>(Arrays.asList("1-12", "13-24", "25-36"));
+    double playerBet;
     public RouletteGame(){
-        this.board = new RouletteBoard();
-//        while(true){ // this goes in run()?
-//            currentNumber = spin();
-//        }
+//        this.board = new RouletteBoard();
     }
 
     public String spin(){
-        Random random = new Random();
-        allNumbers.add("00");
-        for (int i = 0; i <= 36; i++){
-            allNumbers.add(Integer.toString(i));
-        }
-        return allNumbers.get(random.nextInt());
+        return allNumbers.get(random.nextInt(36) + 1);
     }
 
     public boolean isRed(String currentNumber) {
@@ -39,7 +34,7 @@ public class RouletteGame implements GameInterface, GamblingInterface {
             return false;
         }
         else{
-            return board.redNumbers.contains(Integer.valueOf(currentNumber));
+            return redNumbers.contains(Integer.valueOf(currentNumber));
         }
     }
 
@@ -88,20 +83,82 @@ public class RouletteGame implements GameInterface, GamblingInterface {
         }
     }
 
+    public boolean checkCondition(String condition){
+        switch (condition) {
+            case "1-12":
+                return isFirstThird(currentNumber);
+            case "13-24":
+                return isSecondThird(currentNumber);
+            case "25-36":
+                return isLastThird(currentNumber);
+            case "1-18":
+                return isFirstHalf(currentNumber);
+            case "19-36":
+                return !isFirstHalf(currentNumber);
+            case "RED":
+                return isRed(currentNumber);
+            case "BLACK":
+                return !isRed(currentNumber);
+            case "ODD":
+                return isOdd(currentNumber);
+            case "EVEN":
+                return !isOdd(currentNumber);
+        }
+        return false;
+    }
+
+    public double getWinnings(String condition){
+        if (TripleMultiplier.contains(condition)){
+            return playerBet * 3;
+        }
+        else{
+            return playerBet * 2;
+        }
+    }
+
     @Override
     public boolean add(PlayerInterface player) {
-
-        return false;
+        players.add((RoulettePlayer) player);
+        return true;
     }
 
     @Override
     public boolean remove(PlayerInterface player) {
-        return false;
+        players.remove((RoulettePlayer) player);
+        return true;
     }
 
     @Override
     public void run() {
-
+        Scanner scan = new Scanner(System.in);
+        while(true){
+            System.out.println("          WELCOME TO ROULETTE!");
+            System.out.println("         [1-12] [13-24] [25-36]");
+            System.out.println("[1-18] [EVEN] [RED] [BLACK] [ODD] [19-36]");
+            System.out.println("                [QUIT]\n");
+            // players bet
+            System.out.println("CHOOSE A CONDITION: ");
+            String condition = scan.next().toUpperCase();
+            if (condition.equals("QUIT")){
+                System.out.println("YOUR NEW BALANCE IS: " + players.get(0).getArcadeAccount().getBalance());
+                System.out.println("GOODBYE!");
+                break;
+            }
+            System.out.println("PLACE YOUR BET: ");
+            this.playerBet = scan.nextDouble();
+            players.get(0).placeBets(playerBet);
+            this.currentNumber = spin();
+            System.out.println("THE NUMBER IS: " + currentNumber);
+            // determine win or lose round
+            if (checkCondition(condition)){
+                System.out.println("YOU WON $" + getWinnings(condition) + "!\n");
+                players.get(0).getArcadeAccount().setBalance(players.get(0).getArcadeAccount().getBalance() + getWinnings(condition));
+            }
+            else {
+                System.out.println("YOU LOST!");
+                players.get(0).getArcadeAccount().setBalance(players.get(0).getArcadeAccount().getBalance() - playerBet);
+            }
+        }
     }
 
     @Override
@@ -111,11 +168,6 @@ public class RouletteGame implements GameInterface, GamblingInterface {
 
     @Override
     public boolean playAgain(String prompt) {
-        return false;
-    }
-
-    @Override
-    public boolean placeBets(int moneyToBet) {
         return false;
     }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RoulettePlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RoulettePlayer.java
@@ -1,21 +1,28 @@
 package com.github.zipcodewilmington.casino.games.roulette;
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.GamblingInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
-public class RoulettePlayer implements PlayerInterface {
-
-    public RoulettePlayer(){
-
+public class RoulettePlayer implements PlayerInterface, GamblingInterface {
+    CasinoAccount account;
+    double bet;
+    public RoulettePlayer(CasinoAccount account){
+        this.account = account;
     }
 
     @Override
     public CasinoAccount getArcadeAccount() {
+        return account;
+    }
+
+    @Override
+    public Boolean play() {
         return null;
     }
 
     @Override
-    public <SomeReturnType> SomeReturnType play() {
-        return null;
+    public double placeBets(double moneyToBet) {
+        return this.bet = moneyToBet;
     }
 }
